233e5c1cd619cf834830843aa3d0cc0efab22848
eunaumtenhoid <eunaumtenhoid@outlook.com>
2018-08-26 09:20:40 -0300

    UPDATE

--- language/pt_br/common.php
+++ language/pt_br/common.php
@@ -2,10 +2,10 @@
 /**
  *
  * Post Count Requirements extension for the phpBB software package
- * 
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.0] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2017, kinerity, https://www.acsyste.com
  * @license GNU General Public License, version 2 (GPL-2.0)
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.0] (https://github.com/phpBBTraducoes)
+ * 
  */
 
 /**
